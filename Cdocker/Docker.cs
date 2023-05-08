f8716f3105cf1657447b29d0319923e9851a971c
Nath Thornton <Nathanmwt@hotmail.co.uk>
2023-05-08 22:18:17 +0100

    Add project files.

---
+++ Cdocker/Docker.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Docker
+{
+    private const int BufferSize = 64 * 1024; // 64KB
+
+    public static void Create(string filePath)
+    {
+        // Check if the file exists
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"Error: File '{filePath}' does not exist");
+            return;
+        }
+
+        // Determine the file extension
+        var extension = Path.GetExtension(filePath);
+        if (extension != ".cs" && extension != ".java" && extension != ".lua")
+        {
+            Console.WriteLine($"Error: File extension '{extension}' is not supported");
+            return;
+        }
+
+        // Read the file contents
+        var fileBytes = File.ReadAllBytes(filePath);
+
+        // Compile the file
+        var compiler = GetCompiler(extension);
+        var compiledBytes = compiler(fileBytes);
+
+        // Compress the file
+        var compressedBytes = Compress(compiledBytes);
+
+        // Encrypt the file
+        var encryptedBytes = Encrypt(compressedBytes);
+
+        // Save the encrypted file as a .module file
+        var moduleFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".module");
+        File.WriteAllBytes(moduleFilePath, encryptedBytes);
+        Console.WriteLine($"Module file created: {moduleFilePath}");
+    }
+
+    private static Func<byte[], byte[]> GetCompiler(string extension)
+    {
+        switch (extension)
+        {
+            case ".cs":
+                return CompileCs;
+            case ".java":
+                return CompileJava;
+            case ".lua":
+                return CompileLua;
+            default:
+                throw new Exception($"Unsupported extension: {extension}");
+        }
+    }
+
+    private static byte[] CompileCs(byte[] fileBytes)
+    {
+        // TODO: Implement C# compiler logic
+        Console.WriteLine("Compiling C# file...");
+        return fileBytes;
+    }
+
+    private static byte[] CompileJava(byte[] fileBytes)
+    {
+        // TODO: Implement Java compiler logic
+        Console.WriteLine("Compiling Java file...");
+        return fileBytes;
+    }
+
+    private static byte[] CompileLua(byte[] fileBytes)
+    {
+        // TODO: Implement Lua compiler logic
+        Console.WriteLine("Compiling Lua file...");
+        return fileBytes;
+    }
+
+    private static byte[] Compress(byte[] inputBytes)
+    {
+        using var inputStream = new MemoryStream(inputBytes);
+        using var outputStream = new MemoryStream();
+        using var gzipStream = new GZipStream(outputStream, CompressionLevel.Optimal);
+        inputStream.CopyTo(gzipStream);
+        return outputStream.ToArray();
+    }
+
+    private static byte[] Encrypt(byte[] inputBytes)
+    {
+        using var aes = new AesManaged();
+        aes.KeySize = 128; // set the key size to 128 bits
+        aes.GenerateIV();
+        aes.GenerateKey();
+        var key = aes.Key;
+        var iv = aes.IV;
+        using var encryptor = aes.CreateEncryptor(key, iv);
+        using var inputStream = new MemoryStream(inputBytes);
+        using var outputStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write);
+        inputStream.CopyTo(cryptoStream);
+        cryptoStream.FlushFinalBlock();
+        var encryptedBytes = outputStream.ToArray();
+        using var outputMemoryStream = new MemoryStream();
+        using var binaryWriter = new BinaryWriter(outputMemoryStream);
+        binaryWriter.Write(key.Length);
+        binaryWriter.Write(key);
+        binaryWriter.Write(iv.Length);
+        binaryWriter.Write(iv);
+        binaryWriter.Write(encryptedBytes.Length);
+        binaryWriter.Write(encryptedBytes);
+        return outputMemoryStream.ToArray();
+    }
+
+    public static void Run(string moduleFilePath)
+    {
+        // Check if the file exists
+        if (!File.Exists(moduleFilePath))
+        {
+            Console.WriteLine($"Error: File '{moduleFilePath}' does not exist");
+            return;
+        }
+
+        // Read the encrypted file contents
+        var encryptedBytes = File.ReadAllBytes(moduleFilePath);
+
+        // Decrypt the file
+        var decryptedBytes = Decrypt(encryptedBytes);
+
+        // Execute the module program and capture the output
+        using var outputStream = new MemoryStream();
+        Console.SetOut(new StreamWriter(outputStream));
+        ExecuteModule(decryptedBytes);
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+
+        // Print the output to the console
+        var outputString = Encoding.UTF8.GetString(outputStream.ToArray());
+        Console.Write(outputString);
+    }
+
+    public static byte[] Decrypt(byte[] inputBytes)
+    {
+        using var aes = new AesManaged();
+        aes.KeySize = 128; // set the key size to 128 bits
+        aes.Padding = PaddingMode.PKCS7; // set the padding mode to PKCS7
+        using var inputStream = new MemoryStream(inputBytes);
+        var keyLengthBytes = new byte[sizeof(int)];
+        inputStream.Read(keyLengthBytes, 0, sizeof(int));
+        var keyLength = BitConverter.ToInt32(keyLengthBytes, 0);
+        var ivLengthBytes = new byte[sizeof(int)];
+        inputStream.Read(ivLengthBytes, 0, sizeof(int));
+        var ivLength = BitConverter.ToInt32(ivLengthBytes, 0);
+        var key = new byte[keyLength];
+        inputStream.Read(key, 0, keyLength);
+        var iv = new byte[ivLength];
+        inputStream.Read(iv, 0, ivLength);
+        var encryptedBytes = new byte[inputStream.Length - inputStream.Position];
+        inputStream.Read(encryptedBytes, 0, encryptedBytes.Length);
+        using var decryptor = aes.CreateDecryptor(key, iv);
+        using var outputStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(new MemoryStream(encryptedBytes), decryptor, CryptoStreamMode.Read);
+        cryptoStream.CopyTo(outputStream);
+        return outputStream.ToArray();
+    }
+
+    private static void ExecuteModule(byte[] moduleBytes)
+    {
+        // TODO: Implement module execution logic
+        Console.WriteLine("Executing module...");
+    }
+}
---
+++ Cdocker/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Cdocker || beta version";
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        while (true)
+        {
+            Console.Write("Enter command: ");
+            var input = Console.ReadLine().Trim();
+            if (input.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+            {
+                break;
+            }
+            else if (input.StartsWith("create "))
+            {
+                var filePath = input.Substring("create ".Length).Trim();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Docker.Create(filePath);
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (input.StartsWith("run "))
+            {
+                var moduleFilePath = input.Substring("run ".Length).Trim();
+                RunModule(moduleFilePath);
+            }
+            else
+            {
+                Console.WriteLine("Unknown command");
+            }
+        }
+    }
+
+    private static void RunModule(string moduleFilePath)
+    {
+        // Check if the file exists
+        if (!File.Exists(moduleFilePath))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error: File '{moduleFilePath}' does not exist");
+            Console.ForegroundColor = ConsoleColor.Green;
+            return;
+        }
+
+        // Read the encrypted bytes from the file
+        var encryptedBytes = File.ReadAllBytes(moduleFilePath);
+
+        // Decrypt the bytes
+        var decryptedBytes = Docker.Decrypt(encryptedBytes);
+
+        // Create a temporary exe file
+        var tempExePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".exe");
+        File.WriteAllBytes(tempExePath, decryptedBytes);
+
+        // Redirect the output of the exe file to the console
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = tempExePath,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+        Console.ForegroundColor = ConsoleColor.White;
+        process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+        process.ErrorDataReceived += (sender, args) => Console.Error.WriteLine(args.Data);
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+        Console.ForegroundColor = ConsoleColor.Green;
+        // Delete the temporary exe file
+        File.Delete(tempExePath);
+    }
+}
